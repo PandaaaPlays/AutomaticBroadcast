b90b4e3964bd49a312c9f70d4dae8b552fdff826
PandaaaPlays <etienne.toupin03@gmail.com>
2024-04-06 21:57:09 -0400

    Add support for Disabling Toggle

--- src/main/java/ca/pandaaa/automaticbroadcast/AutomaticBroadcast.java
+++ src/main/java/ca/pandaaa/automaticbroadcast/AutomaticBroadcast.java
@@ -10,6 +10,7 @@ import ca.pandaaa.automaticbroadcast.utils.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -24,14 +25,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AutomaticBroadcast extends JavaPlugin {
-    // Generates the broadcasts.yml file //
     private File broadcastsFile;
     private FileConfiguration broadcastsConfig;
-    // Generates the broadcast-toggles.yml file //
     private File togglesFile;
     private FileConfiguration togglesConfig;
-
-    // Attributes //
     private static AutomaticBroadcast plugin;
     private ConfigManager configManager;
     private BroadcastManager broadcastManager;
@@ -39,33 +36,31 @@ public class AutomaticBroadcast extends JavaPlugin {
     private List<Broadcast> broadcastList;
     private BroadcastToggle broadcastToggle;
 
-    // What should happen when the plugin enables //
+    public List<Broadcast> getBroadcastList() {
+        return broadcastList;
+    }
+
+    public BroadcastToggle getBroadcastToggle() {
+        return broadcastToggle;
+    }
+
     @Override
     public void onEnable() {
-        // Plugin initialization //
         plugin = this;
-
-        // BStats initialization //
         int pluginId = 13749;
         Metrics metrics = new Metrics(this, pluginId);
 
-        // Configurations initialization //
         saveDefaultConfigurations();
         loadConfigurations();
         configManager = new ConfigManager(getConfig(), broadcastsConfig, togglesConfig);
-
-        // Changes the manager with a new one (because the configurations might have changed) //
         broadcastManager = new BroadcastManager(createBroadcastList());
 
-        // Creates the command and sync the listener //
         getCommandsAndListeners();
 
         broadcastToggle = new BroadcastToggle(configManager);
 
-        // Starts the broadcasting //
         startBroadcasting();
 
-        // Console message when the plugin is fully enabled //
         getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "   &3_____  &b_____"));
         getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "  &3|  _  |&b| __  |  &3Auto&bmatic&8Broad&7cast"));
         getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "  &3|     |&b| __ -|    &7Version " + getDescription().getVersion()));
@@ -73,15 +68,16 @@ public class AutomaticBroadcast extends JavaPlugin {
         getServer().getConsoleSender().sendMessage("");
     }
 
-    // Saves the online players' toggle status on disabled of the plugin //
     @Override
     public void onDisable() {
+        if (configManager.isToggleDisabled())
+            return;
+
         for(Player player : Bukkit.getOnlinePlayers()) {
             broadcastToggle.saveBroadcastToggle(player);
         }
     }
 
-    // Saves the default configuration files (creates them in the case that they don't already exist) //
     private void saveDefaultConfigurations() {
         this.saveDefaultConfig();
         broadcastsFile = new File(getDataFolder(), "broadcasts.yml");
@@ -89,14 +85,18 @@ public class AutomaticBroadcast extends JavaPlugin {
             saveResource("broadcasts.yml", false);
         broadcastsConfig = new YamlConfiguration();
 
+        if(configManager.isToggleDisabled())
+            return;
         togglesFile = new File(getDataFolder(), "broadcast-toggles.yml");
         if (!togglesFile.exists())
             saveResource("broadcast-toggles.yml", false);
         togglesConfig = new YamlConfiguration();
     }
 
-    // Saves the broadcast-toggles.yml file //
     public void saveBroadcastToggles() {
+        if (configManager.isToggleDisabled())
+            return;
+
         try {
             togglesConfig.save(togglesFile);
         } catch (Exception exception) {
@@ -104,97 +104,76 @@ public class AutomaticBroadcast extends JavaPlugin {
         }
     }
 
-    // Tries to load the broadcasts.yml and broadcast-toggles.yml files //
-    private void loadConfigurations() {
-        try {
-            broadcastsConfig.load(broadcastsFile);
-        } catch (IOException | InvalidConfigurationException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            togglesConfig.load(togglesFile);
-        } catch (IOException | InvalidConfigurationException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Creates or changes the command //
-    private void getCommandsAndListeners() {
-        getCommand("AutomaticBroadcast").setExecutor(new Commands());
-        getCommand("AutomaticBroadcast").setTabCompleter(new TabCompletion());
-        getServer().getPluginManager().registerEvents(broadcastManager, this);
-    }
-
-    // Returns the plugin //
     public static AutomaticBroadcast getPlugin() {
         return plugin;
     }
 
-    // Returns the configuration manager //
     public ConfigManager getConfigManager() {
         return configManager;
     }
 
-    // Starts the broadcasting... //
-    private void startBroadcasting() {
-        // Stops the runnable if it was already instantiated //
-        if (automaticBroadcastTask != null) automaticBroadcastTask.cancel();
-
-        // Generates a runnable that executes the following code in loop //
-        automaticBroadcastTask = new BukkitRunnable() {
-            @Override
-            public void run() {
-                broadcastManager.automaticBroadcast();
-            }
-            // Time between every interval (in seconds) //
-        }.runTaskTimer(plugin, 0, 20L * configManager.getTimeBetweenMessages());
-    }
-
-    // Reloads the configurations //
     public void reloadConfig(CommandSender sender) {
-        // Deletes the config data (not in the file but in the program) //
         plugin.reloadConfig();
-        // Replaces the messages data by the ones from the file //
         broadcastsConfig = YamlConfiguration.loadConfiguration(broadcastsFile);
         togglesConfig = YamlConfiguration.loadConfiguration(togglesFile);
 
-        // Replaces the commands and attributes //
         configManager = new ConfigManager(getConfig(), broadcastsConfig, togglesConfig);
         broadcastManager = new BroadcastManager(createBroadcastList());
-        broadcastToggle = new BroadcastToggle(configManager);
+        if (!configManager.isToggleDisabled())
+            broadcastToggle = new BroadcastToggle(configManager);
         getCommandsAndListeners();
 
-        // Sends the confirmation message to the command executor //
         sender.sendMessage(configManager.getPluginReloadMessage());
-        // Starts the broadcasting //
         startBroadcasting();
     }
 
-    // Creates a list of broadcast for the broadcasts configured in the broadcasts.yml file //
     private List<Broadcast> createBroadcastList() {
         List<Broadcast> broadcastList = new ArrayList<>();
         String[] broadcastTitles = configManager.getBroadcastTitles();
-        // If there are broadcast (not 0) //
-        if (broadcastTitles.length != 0) {
-            for (String title : broadcastTitles) {
-                Broadcast broadcast = new Broadcast(title, configManager.getBroadcastMessagesList(title),
-                        configManager.getBroadcastSound(title), configManager.getBroadcastHoverList(title),
-                        configManager.getBroadcastClick(title));
-                broadcastList.add(broadcast);
-            }
+
+        for (String title : broadcastTitles) {
+            Broadcast broadcast = new Broadcast(title, configManager.getBroadcastMessagesList(title),
+                    configManager.getBroadcastSound(title), configManager.getBroadcastHoverList(title),
+                    configManager.getBroadcastClick(title));
+            broadcastList.add(broadcast);
         }
         this.broadcastList = broadcastList;
         return broadcastList;
     }
 
-    // Returns the list of broadcasts created on start //
-    public List<Broadcast> getBroadcastList() {
-        return broadcastList;
+    private void startBroadcasting() {
+        if (automaticBroadcastTask != null) automaticBroadcastTask.cancel();
+
+        automaticBroadcastTask = new BukkitRunnable() {
+            @Override
+            public void run() {
+                broadcastManager.automaticBroadcast();
+            }
+        }.runTaskTimer(plugin, 0, 20L * configManager.getTimeBetweenMessages());
     }
 
-    // Returns the toggle manager of the plugin //
-    public BroadcastToggle getBroadcastToggle() {
-        return broadcastToggle;
+    private void loadConfigurations() {
+        try {
+            broadcastsConfig.load(broadcastsFile);
+        } catch (IOException | InvalidConfigurationException exception) {
+            System.out.println(exception);
+        }
+
+        try {
+            if(!configManager.isToggleDisabled())
+                togglesConfig.load(togglesFile);
+        } catch (IOException | InvalidConfigurationException exception) {
+            System.out.println(exception);
+        }
+    }
+
+    private void getCommandsAndListeners() {
+        PluginCommand command = getCommand("AutomaticBroadcast");
+        if(command == null)
+            return;
+
+        command.setExecutor(new Commands());
+        command.setTabCompleter(new TabCompletion());
+        getServer().getPluginManager().registerEvents(broadcastManager, this);
     }
 }
--- src/main/java/ca/pandaaa/automaticbroadcast/utils/ConfigManager.java
+++ src/main/java/ca/pandaaa/automaticbroadcast/utils/ConfigManager.java
@@ -1,6 +1,7 @@
 package ca.pandaaa.automaticbroadcast.utils;
 
 import org.bukkit.Sound;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
@@ -8,59 +9,48 @@ import java.util.List;
 import java.util.Set;
 
 public class ConfigManager {
-    // Attributes //
     private final FileConfiguration configuration;
     private final FileConfiguration broadcasts;
     private final FileConfiguration toggles;
 
-    // Constructor //
     public ConfigManager(FileConfiguration configuration, FileConfiguration broadcasts, FileConfiguration toggles) {
         this.configuration = configuration;
         this.broadcasts = broadcasts;
         this.toggles = toggles;
     }
 
-    // Returns the time between every broadcast (in ticks.. 20 ticks is 1 sec) //
     public int getTimeBetweenMessages() {
         return configuration.getInt("time-between-messages");
     }
 
-    // Returns true if the broadcasts should be sent randomly //
     public boolean getRandom() {
         return configuration.getBoolean("random");
     }
 
-    // Returns true if the exempt permission is enabled //
     public boolean getExemptPermission() {
         return configuration.getBoolean("exempt-permission");
     }
 
-    // Returns a list of the disabled world(s) //
     public List<String> getDisabledWorlds() {
         return configuration.getStringList("disabled-worlds");
     }
 
-    // Returns a list of the exempted player(s) //
     public List<String> getExemptedPlayers() {
         return configuration.getStringList("exempted-players");
     }
 
-    // Returns the unknown command message //
     public String getUnknownCommandMessage() {
         return Utils.applyFormat(configuration.getString("unknown-command"));
     }
 
-    // Returns the no permission message //
     public String getNoPermissionMessage() {
         return Utils.applyFormat(configuration.getString("no-permission"));
     }
 
-    // Returns the plugin reload command message //
     public String getPluginReloadMessage() {
         return Utils.applyFormat(configuration.getString("plugin-reload"));
     }
 
-    // Returns the plugin toggle command messages //
     public String getBroadcastToggleMessage(String type) {
         if(type.equals("on"))
             return Utils.applyFormat(configuration.getString("toggle-on-message"));
@@ -68,32 +58,28 @@ public class ConfigManager {
             return Utils.applyFormat(configuration.getString("toggle-off-message"));
     }
 
-    // Returns a list of all the existing broadcasts (Staff, Discord, etc.) //
     public String[] getBroadcastTitles() {
-        // Creates a Set with all the titles of the messages directly from the "broadcasts.yml" file //
-        Set<String> broadcastTitlesSet = broadcasts.getConfigurationSection("broadcasts").getKeys(false);
-        // Creates a list of the size of the "broadcastTitlesSet" Set //
-        String[] broadcastTitlesList = new String[broadcastTitlesSet.size()];
-        // Changes the created array to set the collected titles (Set) inside it //
-        return broadcastTitlesSet.toArray(broadcastTitlesList);
+        ConfigurationSection broadcastsTitleSection = broadcasts.getConfigurationSection("broadcasts");
+        if(broadcastsTitleSection == null)
+            return new String[0];
+
+        Set<String> broadcastsTitleSet = broadcastsTitleSection.getKeys(false);
+        String[] broadcastTitlesList = new String[broadcastsTitleSet.size()];
+        return broadcastsTitleSet.toArray(broadcastTitlesList);
     }
 
-    // Returns a list of all the messages in a broadcast //
     public List<String> getBroadcastMessagesList(String broadcastTitle) {
         return broadcasts.getStringList("broadcasts." + broadcastTitle + ".messages");
     }
 
-    // Returns the broadcast "click" message/command/suggestion string if applicable //
     public String getBroadcastClick(String broadcastTitle) {
         return broadcasts.getString("broadcasts." + broadcastTitle + ".click");
     }
 
-    // Returns the broadcast "hover" message(s) list if applicable //
     public List<String> getBroadcastHoverList(String broadcastTitle) {
         return broadcasts.getStringList("broadcasts." + broadcastTitle + ".hover");
     }
 
-    // Tries to get the broadcast sound value (if it exist...) //
     public Sound getBroadcastSound(String broadcastTitle) {
         try {
             return Sound.valueOf(broadcasts.getString("broadcasts." + broadcastTitle + ".sound"));
@@ -110,5 +96,9 @@ public class ConfigManager {
         if(!toggles.getKeys(false).contains(player.getUniqueId().toString()))
             return true;
         return toggles.getBoolean(player.getUniqueId() + ".toggled");
+    }
+
+    public boolean isToggleDisabled() {
+        return configuration.getBoolean("disable-toggle");
     }
 }
